b83ca46911a95c8eba6def0296aad48f20fb6ae6
brugger <kim.brugger@uib.no>
2020-05-07 10:30:14 +0200

    ehost status script to be used for monitoring with telegraf

---
+++ bin/ehos_status.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+#
+#
+#
+#
+# Kim Brugger (20 Sep 2018), contact: kim@brugger.dk
+
+import pprint
+import sys
+
+import ehos.utils
+
+pp = pprint.PrettyPrinter(indent=4)
+import argparse
+import socket
+from munch import Munch
+
+# python3+ is broken on centos 7, so add the /usr/local/paths by hand
+#sys.path.append("/usr/local/lib/python{}.{}/site-packages/".format( sys.version_info.major, sys.version_info.minor))
+#sys.path.append("/usr/local/lib64/python{}.{}/site-packages/".format( sys.version_info.major, sys.version_info.minor))
+
+
+
+import ehos
+import ehos.htcondor
+import ehos.instances
+
+
+def get_hostname() -> str:
+    return socket.getfqdn()
+
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='ehos_status: print ehos status in telegraf format')
+    parser.add_argument('config_files', metavar='config-files', nargs='+', help="yaml formatted config files", default=ehos.utils.find_config_file('ehos.yaml'))
+    args = parser.parse_args()
+
+    logger.init(name='ehosd' )
+    logger.set_log_level( 0 )
+
+
+    config = ehos.utils.get_configurations(args.config_files)
+    condor = ehos.htcondor.Condor()
+    clouds = ehos.connect_to_clouds( config )
+    instances = ehos.instances.Instances()
+    instances.add_clouds( clouds )
+
+    # get the current number of nodes
+    instances.update(condor.nodes())
+    nodes = instances.node_state_counts()
+    nodes = Munch(nodes[ 'all' ])
+    jobs   = condor.job_counts()
+
+    hostname = get_hostname()
+
+    print("condor,host={} jobs_total={},jobs_idle={},jobs_running={},jobs_suspended={}".format( hostname, jobs.get('job_total', 0), jobs.get('job_idle',0), jobs.get('job_running'), 0), jobs.get('job_suspended'), 0))
+    print("ehos,host={} nodes_total={},nodes_idle={},nodes_busy={}".format( hostname, nodes.get('node_total', 0), nodes.get('node_idle',0), nodes.get('node_busy'), 0))
+
+if __name__ == '__main__':
+    main()
+else:
+    print("Not to be run as a library")
+    sys.exit( 1 )
+
+
+
